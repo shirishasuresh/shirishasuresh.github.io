c9d2b762adb3e0a3242d8cb03b30da395d4cfffb
G Shirisha <75423483+shirishasuresh@users.noreply.github.com>
2025-12-05 11:51:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle navbar for mobile
+const toggleButton = document.getElementById('toggle-button');
+const navbarLinks = document.getElementById('navbar-links');
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
+
